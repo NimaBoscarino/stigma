dccb64611db9cc2f37a1c68f9793c50b0024799d
Nima Boscarino <nboscari@sfu.ca>
2019-05-31 21:56:31 -0700

    api route for /artists/username

--- rails-back-end/app/controllers/api/artists_controller.rb
+++ rails-back-end/app/controllers/api/artists_controller.rb
@@ -1,7 +1,14 @@
 class Api::ArtistsController < ApplicationController
   def index
-
     artists = Artist.all
+
+    render :json => {
+      artists: artists
+    }
+  end
+
+  def show
+    artists = Artist.find_by(username: params['id'])
 
     render :json => {
       artists: artists

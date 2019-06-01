4275de30ba9307551bf821af2901e32a902a4e23
Nima Boscarino <nboscari@sfu.ca>
2019-06-01 10:59:16 -0700

    Bring in the ants! (design)

--- react-front-end/src/components/ArtistCard.js
+++ react-front-end/src/components/ArtistCard.js
@@ -1,62 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import { Link } from "react-router-dom";
+import { Card, Icon } from 'antd';
+const { Meta } = Card;
 
-const styles = {
-  card: {
-    maxWidth: 345,
-    margin: '5px',
-  },
-  media: {
-    height: 140,
-  },
-};
+const LinkToProfile = ({artist}) => (
+  <Link to={`/artists/${artist.username}`}>
+    <Icon type="calendar" />
+  </Link>
+)
 
-function MediaCard(props) {
-  const { classes, artist} = props;
+export default (props) => {
+  const { artist } = props;
   console.log(props)
   return (
-    <Card className={classes.card}>
-      <CardActionArea
-        component={Link}
-        to={`/artists/${artist.username}`}
-      >
-        <CardMedia
-          className={classes.media}
-          image={artist.avatar}
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {artist.name}
-          </Typography>
-          <Typography component="p">
-            {artist.username}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          See Profile
-        </Button>
-        <Button size="small" color="primary">
-          Follow
-        </Button>
-      </CardActions>
+    <Card
+      hoverable
+      style={{ width: 240 }}
+      cover={
+        <img alt="example" src={artist.avatar} />
+      }
+      actions={[
+        <LinkToProfile artist={artist} />,
+        <Icon type="heart" />
+      ]}
+    >
+      <Meta title={artist.name} description={artist.username} />
     </Card>
   );
 }
-
-MediaCard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(MediaCard);
